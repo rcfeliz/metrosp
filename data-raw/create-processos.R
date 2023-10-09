2f4ab98301c7a4b84fdd530a361a6de609cdd750
rcfeliz <ric.feliz@gmail.com>
2023-10-09 02:01:56 -0300

    pega tempo total do contrato

--- data-raw/create-processos.R
+++ data-raw/create-processos.R
@@ -65,15 +65,13 @@ aux_status <- processos_bruto |>
   dplyr::ungroup() |>
   tidyr::nest(.by = c(id_processo, estacao, dt_inicio, status), .key = "fases") |>
   dplyr::mutate(status = ifelse(is.na(estacao), NA_character_, status)) |>
-  dplyr::arrange(id_processo) |>
-  dplyr::select(id_processo, status)
+  dplyr::arrange(id_processo)
 
-# extrair valores ---------------------------------------------------------
+# extrair valores (R$) ---------------------------------------------------------
 
 path_valor <- "data-raw/pdf/valor"
 
-rgx_reais <- "\\b(?:\\d{1,3}(?:\\.\\d{3})*,\\d{2}|\\d{1,3},\\d{2})\\b"
-
+# download
 aux_status |>
   dplyr::filter(status == "sucesso") |>
   tidyr::unnest(fases) |>
@@ -85,6 +83,9 @@ aux_status |>
   dplyr::select(id_processo, link_arq) |>
   dplyr::group_split(id_processo) |>
   purrr::map(extract_pdf, path = path_valor)
+
+# parse
+rgx_reais <- "\\b(?:\\d{1,3}(?:\\.\\d{3})*,\\d{2}|\\d{1,3},\\d{2})\\b"
 
 aux_valores <- tibble::tibble(
   id_processo = fs::dir_ls(path_valor) |>
@@ -107,16 +108,45 @@ aux_valores <- tibble::tibble(
     unlist()
 )
 
+
+# extrair tempo -----------------------------------------------------------
+
+path_tempo <- "data-raw/pdf/tempo"
+
+aux_status |>
+  dplyr::filter(status == "sucesso") |>
+  tidyr::unnest(fases) |>
+  dplyr::filter(stringr::str_detect(fase, "HOMOLOG")) |>
+  dplyr::group_by(id_processo) |>
+  dplyr::filter(id_fase == min(id_fase, na.rm = TRUE)) |>
+  dplyr::ungroup() |>
+  tidyr::unnest(arquivos) |>
+  dplyr::select(id_processo, link_arq) |>
+  dplyr::group_split(id_processo) |>
+  purrr::map(extract_pdf, path = path_tempo)
+
+aux_tempo <- tibble::tibble(
+  id_processo = fs::dir_ls(path_valor) |>
+    stringr::str_extract("[0-9]+"),
+  tempo_contrato = fs::dir_ls(path_tempo) |>
+    purrr::map(pdftools::pdf_text) |>
+    purrr::map(~purrr::keep(.x, stringr::str_detect(.x, "VIGÊNCIA/PRAZOS"))) |>
+    purrr::discard(~ length(.x) == 0) |>
+    paste0() |>
+    stringr::str_extract("(?<=O prazo de vigência do presente Contrato de Concessão é de )[0-9]+") |>
+    paste0(" anos")
+)
 # processos ---------------------------------------------------------------
 
 processos <- list(
   processos_bruto,
   aux_dt_inicio,
-  aux_status,
-  aux_valores
+  dplyr::select(aux_status, id_processo, status),
+  aux_valores,
+  aux_tempo
 ) |>
   purrr::reduce(dplyr::left_join, by = "id_processo") |>
-  dplyr::select(id_processo, estacao, dt_inicio, status, valor_total, valor_mensal, fases)
+  dplyr::select(id_processo, estacao, dt_inicio, status, valor_total, valor_mensal, tempo_contrato, fases)
 
 processos |>
   dplyr::select(dplyr::contains("valor")) |>
